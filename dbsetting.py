4c17727d75179777f6c24cdffec56c00df4d4ad5
Yutaro Onishi <onishiyutaro19900816@gmail.com>
2018-09-04 23:35:25 +0900

    DB接続情報をheroku用に変更

--- dbsetting.py
+++ dbsetting.py
@@ -4,7 +4,8 @@ from sqlalchemy.orm import sessionmaker
 
 #DB作成用
 Base = declarative_base()
-conn = sa.create_engine('postgresql://y_onishi:@localhost/test1')
+conn = sa.create_engine('postgres://wvgugralmjwxxs:7f1f08506d3d04801067bb02ee92f806c8aac35bbba1680ef00ab9e59b7b024e@ec2-54-163-245-44.compute-1.amazonaws.com:5432/d6qg27fj5opkk1')
+#conn = sa.create_engine('postgresql://y_onishi:@localhost/test1')
 
 #レコードクラス（作成するDBの形式）
 class LinkRecord(Base):
